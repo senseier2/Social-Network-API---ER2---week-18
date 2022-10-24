6556a1d4cf494f859740f1c1d7086ff5583913b9
Senfire <eric.mcghee@northwestern.edu>
2022-10-24 14:33:38 -0500

    updating the thought routes to match the new controller routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -6,6 +6,8 @@ getSingleThought,
 createThought,
 updateThought,
 deleteThought,
+createReaction,
+deleteReaction,
 
 } = require('../../controllers/thoughtController');
 
@@ -13,11 +15,19 @@ deleteThought,
 router.route('/')
     .get(getThoughts)
     .post(createThought)
-
+// /api/thoughts/:thoughtId
 router.route('/:thoughtId')
     .get(getSingleThought)
     .put(updateThought)
     .delete(deleteThought)
+
+// /api/thoughts/:thoughtId/reactions
+router.route('./:thoughtId/reactions')
+    .post(createReaction)
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/reactionId')
+    .delete(deleteReaction)
 
 
 module.exports = router;
